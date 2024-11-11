a7ee036b52637fffbfebde98e3ccc4910e4bde67
Marine Van Hees <vanheesmarine@gmail.com>
2024-11-11 23:53:31 +0100

    create components

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
+import { Hero } from '../hero';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgFor,
+    HeroDetailComponent
+],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
